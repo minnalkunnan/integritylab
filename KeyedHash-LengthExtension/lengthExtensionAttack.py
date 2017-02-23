6c98e4c1f974710524928ef45cead0026e1bb4a8
minnalkunnan <minnalkunnan@gmail.com>
2017-02-22 19:00:33 -0800

    Updates

--- KeyedHash-LengthExtension/lengthExtensionAttack.py
+++ KeyedHash-LengthExtension/lengthExtensionAttack.py
@@ -17,18 +17,22 @@ def hash(message, tag):
    print(hex(h4))
    """
    a = sha1.sha(h0, h1, h2, h3, h4, message)
-   print((str(hex(a)))[2:len(str(hex(a))) - 1])
+   return (str(hex(a))[2:len(str(hex(a))) - 1])
+
 
 def attack():
 	message = 'Funny%20names?'
-	padding = '%80'
 	i = 0
 	for i in range(512):
 		zeros = '%00' * i
-		ourMessage = message + '%80' + zeros + '%60' + 'sup'
-		tag = hash('sup', 'b8366a6271aec451046ce8892d308e771662d446')
-		url = 'http://localhost:8080/?who=Costello&what=' + ourMessage + '&mac=' + tag
+		ourMessage = message + '%80' + zeros + '%60'
+		tag = hash('sup', 'e0800a310b4a9b224dd02ba081419440f659d187')
+		url = 'http://localhost:8080/?who=Costello&what=' + ourMessage + '&mac=' + 'e0800a310b4a9b224dd02ba081419440f659d187'
+		#print(url)
 		r = requests.get(url)
-		print(r)
-
+		if 'Invalid signature' in r.text:
+			print "we suck"
+		else:
+			print "deal with it"
+			
 attack()
