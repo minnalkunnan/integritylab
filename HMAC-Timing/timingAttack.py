3f08381f930ad7d6fd626c3e81fb10e863d5f5c0
cthomp18 <code8593@gmail.com>
2017-02-22 21:02:04 -0800

    more

--- HMAC-Timing/timingAttack.py
+++ HMAC-Timing/timingAttack.py
@@ -21,19 +21,24 @@ def attack():
 	url = 'http://localhost:8080/?q=foo&mac=' + ascii_to_hex(''.join(mac))
 	r = requests.get(url)
 
-	for i in range(256):
-		mac[0] = chr(i)
-		url = 'http://localhost:8080/?q=foo&mac=' + ascii_to_hex(''.join(mac))
-		print(url)
-		start_time = time.time()
-		r = requests.get(url)
-		#if 'Invalid signature' not in r.text:
-		#	print 'Success'
-		#	print mac
-		#	break
-		elapsed_time = time.time() - start_time
-		print(elapsed_time)
-		
+	for ind in range(20):
+		for i in range(256):
+			times = []
+			mac[ind] = chr(i)
+			url = 'http://localhost:8080/?q=foo&mac=' + ascii_to_hex(''.join(mac))
+			print(url)
+			start_time = time.time()
+			r = requests.get(url)
+			#if 'Invalid signature' not in r.text:
+			#	print 'Success'
+			#	print mac
+			#	break
+			elapsed_time = time.time() - start_time
+			times.append(elapsed_time)
+		slowest = times.index(max(times))
+		mac[ind] = chr(slowest)
+	
+	print(ascii_to_hex(''.join(mac)))
 
 attack()
 
